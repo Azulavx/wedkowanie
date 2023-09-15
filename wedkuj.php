9a922a172a7bb8f409487f6e7731d3e40fc4562d
azulvx <martynamoczulska2004@gmail.com>
2023-09-15 13:27:45 +0200

    f

---
+++ wedkuj.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="css.css">
+</head>
+<body>
+<header class="portal">
+        <h2>Portal&nbsp;dla&nbsp;wędkarzy&nbsp;</h2>
+    </header>
+    <main>
+        <article>
+            <h1>Ryby drapieżne naszych wód</h1>
+        <div class="ryby">
+            <div class="border3"> 1. Szczupak pływa w rzece Warta-Obrzycko, Wielkopolskie
+                 </div>
+            
+        </article>
+        <section>
+            <p>
+                <img src="ryba.jpg" alt="Sum" style="border: solid 1px #000080; padding: 5px; margin: 100px ;">
+            </p>
+
+            <p><a href="kwerendy.txt" style="text-decoration: none;">Pobierz kwerendy</a>
+            </p>
+        </section>
+        </main>
+            <footer id="jk">
+        <center>                                                              
+            <h4>Stronę&nbsp;wykonała:&nbsp;MARTYNA&nbsp;MOCZULSKA &nbsp;5&nbsp;GP</h4>
+        </center>
+    </footer>
+</body>
+</html>
